f381edca4e784b9e3a271054e53f172ff42d2fb8
Aaron <aaronakohler@gmail.com>
2019-04-30 16:41:37 -0700

    Finish iOS Repository & AppDelegate

---
+++ Homepwner/Models/FixedItemStore.swift
@@ -0,0 +1,23 @@
+//
+//  FixedItemStore.swift
+//  Homepwner
+//
+//  Created by Student Account on 4/30/19.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+class FixedItemStore: ItemRepository{
+    
+    func createItem(item:Item) {
+        allItems.append(item)
+    }
+    
+    var allItems: [Item] = []
+    
+    init() {
+        let newItem = Item(name: "Jetpack", valueInDollars: 2500)
+            createItem(item:newItem)
+        }
+    }
