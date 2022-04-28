04832a57b014af118b1d69970ed521e6ba93684e
othman <othmandjuliarso@gmail.com>
2022-04-28 14:59:41 -0700

    created and completed FixedItemStore class, swapped the app delegate to use FixedItemStore

---
+++ Homepwner/Models/FixedItemStore.swift
@@ -0,0 +1,25 @@
+//
+//  FixedItemStore.swift
+//  Homepwner
+//
+//  Created by Student Account on 4/28/22.
+//  Copyright © 2022 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+class FixedItemStore: ItemRepository {
+    var allItems: [Item] = []
+    
+    init() {
+        let newItem = Item(name: "jetpack", valueInDollars: 2500)
+            createItem(item:newItem)
+    }
+    
+    func createItem(item:Item) {
+        allItems.append(item)
+    }
+    
+    
+
+}
